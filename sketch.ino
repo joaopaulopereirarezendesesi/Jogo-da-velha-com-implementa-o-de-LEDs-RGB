d0ff9968eac7bac43aeeb1a76ab511dbb7b307f4
João Paulo Pereira Rezende <163848013+joaopaulopereirarezendesesi@users.noreply.github.com>
2024-08-08 19:49:19 -0300

    Create sketch.ino

---
+++ sketch.ino
@@ -0,0 +1,27 @@
+#include "ligarLed.h"
+
+PosicaoControl posCtrl;
+
+/*
+  posCtrl.setPosicao(Number of LED you want to light up, color you want);
+
+  colors:
+  1 = Red;
+  2 = Blue;
+  3 = Pink;
+  4 = All leds blues;
+  5 = All leds reds;
+  6 = All leds pinks;
+  7 = not one led.
+
+  LED numbering:
+  Led 1, Led 2, Led 3
+  Led 4, Led 5, Led 6
+  Led 7, Led 8, Led 9.
+*/
+
+void setup() {
+}
+
+void loop() {
+}
